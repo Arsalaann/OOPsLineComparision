707d42ea2957eed487a40408cefa32502e9c1f74
Arsalaann <arsalankhanxk@gmail.com>
2023-05-06 15:24:27 +0530

    Check Equality

--- DefineLine.cs
+++ DefineLine.cs
@@ -4,6 +4,7 @@ namespace OOPsLineComputation
     public class DefineLine
     {
         public int x1,y1,x2,y2;
+        public double area;
         public DefineLine(){
         }
         public DefineLine(int x1,int y1,int x2,int y2){
@@ -11,6 +12,13 @@ namespace OOPsLineComputation
             this.y1 = y1;
             this.x2 = x2;
             this.y2 = y2;
+            this.area=this.CalculateLength();
         }
+
+        public double CalculateLength(){
+            return Math.Sqrt(((this.x2-this.x1)*(this.x2-this.x1))+((this.y2-this.y1)*(this.y2-this.y1)));
+        }
+
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -2,8 +2,15 @@ namespace OOPsLineComputation
 {
     public class Program
     {
+        private int X1=2,X2=4,Y1=6,Y2=8,x1=1,y1=3,x2=5,y2=7; 
         public static void Main(string[] args){
-            DefineLine line=new DefineLine();
+            Program obj=new Program();
+            DefineLine line1=new DefineLine(obj.X1,obj.Y1,obj.X2,obj.Y2);
+            DefineLine line2=new DefineLine(obj.x1,obj.y1,obj.x2,obj.y2);
+            if(line1.area.Equals(line2.area))
+                Console.WriteLine("Lines are Equal");
+            else
+                Console.WriteLine("Lines are not Equal");
         }
     }
 }
